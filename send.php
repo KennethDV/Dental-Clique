a3e3fed4aee88db7b2dcce23fa07d09cdfb1bff3
KennethDV <deverakenneth21@gmail.com>
2023-04-28 01:23:01 +0800

    first commit

---
+++ send.php
@@ -0,0 +1,35 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+if(isset($_POST["send"])){
+    
+    
+    $mail = new PHPMailer(true);
+    
+    $mail->isSMTP();
+    $mail->Host = 'smtp.hostinger.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'kenneth@villamasanori.fun';
+    $mail->Password = 'Kenneth@123';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+    
+    $mail->setFrom('kenneth@villamasanori.fun');
+    
+    $mail->addAddress($_POST["email"]);
+    
+    $mail->isHTML(true);
+    
+    $mail->Subject = $_POST["subject"];
+    $mail->Body = "Thank you for contacting us! We will get back to you shortly. Have a wonderful day " . $_POST['name'];
+    
+    $mail->send();
+    
+    header('Location: index.php');
+}
+?>
