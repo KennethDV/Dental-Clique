2ba7f409c504e5008595b7872b94d0cce0b2402f
KennethDV <deverakenneth21@gmail.com>
2023-06-05 06:10:36 +0800

    db update

--- connection.php
+++ connection.php
@@ -1,9 +1,14 @@
 <?php
 
-$servername = "localhost";
-$userName = "Kenneth";
-$password = "12345";
-$database = "dental_db";
+// $servername = "localhost";
+// $userName = "Kenneth";
+// $password = "12345";
+// $database = "dental_db";
+
+$servername = "mysql.hostinger.in";
+$userName = "u235219407_kenneth";
+$password = "UJNYhW8R";
+$database = "u235219407_dentaldb";
 
 $con = mysqli_connect($servername, $userName, $password, $database);
 ?>
